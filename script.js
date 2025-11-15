21f96c9164dc1d39f3c7d6a09ec1576b180cf29c
JonGudenzi <jongudenzi@gmail.com>
2025-11-15 14:32:07 -0500

    addTask function. render elements

--- script.js
+++ script.js
@@ -5,37 +5,65 @@ const taskList = document.getElementById("taskList");
 
 let tasks = [];
 
-function addTask() {
+function addTask(text) {
+    const taskObj = {
+        text: text,
+        completed: false
+    }
+    tasks.push(taskObj);
+    render();
+    saveTasks();
+}
+
+
+function render(index) {
+taskList.innerHTML = "";
+tasks.forEach(function(task) {
+const li = document.createElement("li");
+li.textContent = task.text;
+li.dataset.index = index;
+if (task.completed) {
+    li.classList.add("completed");
+}
+const deleteBtn = document.createElement("button");
+deleteBtn.textContent = "X";
+li.appendChild(deleteBtn);
+li.addEventListener("click", toggleTask);
+taskList.appendChild(li);
+})
+}
+
+
+function toggleTask() {
 
 }
 
 
-function render(){
+function deleteTask() {
 
 }
 
 
-function toggleTask(){
+function saveTasks() {
 
 }
 
 
-function deleteTask(){
+function loadTasks() {
 
 }
 
+taskForm.addEventListener("submit", handleSubmit);
 
-function saveTasks(){
-
-}
-
-
-function loadTasks(){
-
-}
-
-
-function handleSubmit(){
-
+function handleSubmit(event) {
+    event.preventDefault();
+    const userText = taskInput.value.trim();
+    if (userText === "") {
+        errorMsg.textContent = "Please enter a task";
+    }
+    errorMsg.textContent = "";
+    addTask(userText);
+    taskInput.value = "";
+    taskInput.focus();
 }
 loadTasks();
