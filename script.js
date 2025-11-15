cc085e3608fd87bca56fb63268a30b7b85c778dd
JonGudenzi <jongudenzi@gmail.com>
2025-11-15 14:12:00 -0500

    function skeleton layout

---
+++ script.js
@@ -0,0 +1,41 @@
+const taskForm = document.getElementById("taskForm");
+const taskInput = document.getElementById("taskInput");
+const errorMsg = document.getElementById("errorMsg");
+const taskList = document.getElementById("taskList");
+
+let tasks = [];
+
+function addTask() {
+
+}
+
+
+function render(){
+
+}
+
+
+function toggleTask(){
+
+}
+
+
+function deleteTask(){
+
+}
+
+
+function saveTasks(){
+
+}
+
+
+function loadTasks(){
+
+}
+
+
+function handleSubmit(){
+
+}
+loadTasks();
