943efa6e77e38c23aecf8f8bff21a2c10f8b2ced
JonGudenzi <jongudenzi@gmail.com>
2025-11-15 15:32:11 -0500

    loading local storage

--- script.js
+++ script.js
@@ -16,51 +16,59 @@ function addTask(text) {
 }
 
 function render() {
-taskList.innerHTML = "";
-tasks.forEach(function(task, index) {
-const li = document.createElement("li");
-li.textContent = task.text;
-li.dataset.index = index;
-if (task.completed) {
-    li.classList.add("completed");
-}
-const deleteBtn = document.createElement("button");
-deleteBtn.textContent = "X";
-li.appendChild(deleteBtn);
-li.addEventListener("click", toggleTask);
-deleteBtn.addEventListener("click", function(event){
-event.stopPropagation();
-const li = document.closest("li");
-const index = li.dataset.index;
-deleteTask(index);
-})
-taskList.appendChild(li);
-})
+    taskList.innerHTML = "";
+    tasks.forEach(function (task, index) {
+        const li = document.createElement("li");
+        li.textContent = task.text;
+        li.dataset.index = index;
+        if (task.completed) {
+            li.classList.add("completed");
+        }
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "X";
+        li.appendChild(deleteBtn);
+        li.addEventListener("click", toggleTask);
+        deleteBtn.addEventListener("click", function (event) {
+            event.stopPropagation();
+            const li = deleteBtn.closest("li");
+            const index = li.dataset.index;
+            deleteTask(index);
+        })
+        taskList.appendChild(li);
+    })
 }
 
 function toggleTask(event) {
-const li = event.target;
-const index = li.dataset.index;
-tasks[index].completed = !tasks[index].completed;
-render();
-saveTasks();
+    const li = event.target;
+    const index = li.dataset.index;
+    tasks[index].completed = !tasks[index].completed;
+    render();
+    saveTasks();
 }
 
 
 function deleteTask(index) {
-tasks.splice(index, 1);
-render();
-saveTasks();
+    tasks.splice(index, 1);
+    render();
+    saveTasks();
 }
 
 function saveTasks() {
-const taskString = JSON.stringify(tasks);
-localStorage.setItem("task", taskString);
+    const taskString = JSON.stringify(tasks);
+    localStorage.setItem("task", taskString);
 }
 
 
 function loadTasks() {
-
+    const savedData = localStorage.getItem("task");
+    if (savedData) {
+        const newArray = JSON.parse(savedData);
+        tasks.length = 0;
+        newArray.forEach(function(item) {
+            tasks.push(item);
+        })
+    }
+    render();
 }
 
 taskForm.addEventListener("submit", handleSubmit);
